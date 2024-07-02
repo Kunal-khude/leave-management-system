c4dcc8f520e1d91cdf565b9e575d0e3445a957ea
Donnion99 <donnion99@gmail.com>
2024-07-02 16:51:03 +0530

    make a header ,footer, siderabr and a table for index page

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,8 @@
 <?php
+include '../templates/header.php';
+?>
+
+<?php
 echo "Welcome to Admin";
+include '../templates/sidebar.php';
+?>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <?php
 $server_name = $_SERVER['SERVER_NAME'];
 $base_url = "http://" . $server_name . "/leave-management-system";
-include('./include/db-connection.php');
+include('../include/db-connection.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +13,7 @@ include('./include/db-connection.php');
     <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/bootstrap.min.css">
     <script src="<?php echo $base_url; ?>/assets/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 
 <body>
